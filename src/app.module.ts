ec59337977445e7d2f33b146605b2e6a8646ec7d
makakof <luiz.h.s.silva@ufms.br>
2025-06-27 16:54:44 -0400

    conexão com banco feita

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsuariosModule } from './usuarios/usuarios.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsuariosModule } from "./usuarios/usuarios.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [UsuariosModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [".env"],
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsuariosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
