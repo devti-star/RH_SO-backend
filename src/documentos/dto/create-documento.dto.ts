77e709d7201c9c1ab9fe220b44a3ce0a505e1b5c
JoaoSartoreto <joaovitorsartoreto@hotmail.com>
2025-06-30 14:16:59 -0400

    Criação das entidades e dtos de documentos

--- src/documentos/dto/create-documento.dto.ts
+++ src/documentos/dto/create-documento.dto.ts
@@ -1,1 +1,11 @@
-export class CreateDocumentoDto {}
+import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+
+export class CreateDocumentoDto {
+  @IsNotEmpty()
+  @IsString()
+  caminho: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  requerimentoId: number;
+}
--- src/documentos/entities/documento.entity.ts
+++ src/documentos/entities/documento.entity.ts
@@ -1,9 +1,20 @@
-import { ManyToOne } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
 import { Requerimento } from '../../requerimentos/entities/requerimento.entity';
 
+@Entity('documentos')
 export class Documento {
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column({ length: 255 })
+  caminho: string; // caminho do arquivo (ex: uploads/...)
 
-    @ManyToOne(() => Requerimento, (requerimento) => requerimento.documentos)
-    requerimento: Requerimento;
+  @ManyToOne(() => Requerimento, (requerimento) => requerimento.documentos, { onDelete: 'CASCADE' })
+  requerimento: Requerimento;
 }
