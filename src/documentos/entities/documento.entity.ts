19ea79c8f4d16077edeac4520f90162e373f5fe2
JoaoSartoreto <joaovitorsartoreto@hotmail.com>
2025-06-30 14:30:54 -0400

    fix/adição de herança para classe Atestado

--- src/documentos/entities/documento.entity.ts
+++ src/documentos/entities/documento.entity.ts
@@ -3,11 +3,13 @@ import {
   PrimaryGeneratedColumn,
   Column,
   ManyToOne,
-  CreateDateColumn,
+  ChildEntity,
+  TableInheritance,
 } from 'typeorm';
 import { Requerimento } from '../../requerimentos/entities/requerimento.entity';
 
 @Entity('documentos')
+@TableInheritance({ column: { type: 'varchar', name: 'tipo_documento' } })
 export class Documento {
   @PrimaryGeneratedColumn()
   id: number;
@@ -18,3 +20,18 @@ export class Documento {
   @ManyToOne(() => Requerimento, (requerimento) => requerimento.documentos, { onDelete: 'CASCADE' })
   requerimento: Requerimento;
 }
+
+@ChildEntity()
+export class Atestado extends Documento {
+  @Column({ type: 'boolean', default: false })
+  maior3dias: boolean; // Indica se o atestado é maior que 3 dias
+
+  @Column({ type: 'boolean', default: false })
+  concluido: boolean; // Indica se o atestado foi concluído
+
+  @Column({ length: 255, nullable: true })
+  justificativa: string; // Justificativa para o atestado, se necessário
+
+  // Adicione mais campos que sejam específicos de atestado, se necessário
+}
+
