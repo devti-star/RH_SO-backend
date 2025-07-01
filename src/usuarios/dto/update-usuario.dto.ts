9dd0b99e03c9b2cde36ccc640e89c5c391aa226e
JoaoSartoreto <joaovitorsartoreto@hotmail.com>
2025-07-01 14:49:56 -0400

    feature/ implementação do update do usuario

--- src/usuarios/dto/update-usuario.dto.ts
+++ src/usuarios/dto/update-usuario.dto.ts
@@ -1,4 +1,36 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUsuarioDto } from './create-usuario.dto';
+// src/usuarios/dto/update-usuario.dto.ts
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class UpdateUsuarioDto {
+  @IsOptional()
+  @IsString()
+  nomeCompleto?: string;
+
+  @IsOptional()
+  @IsString()
+  departamento?: string;
+
+  @IsOptional()
+  @IsString()
+  secretaria?: string;
+
+  @IsOptional()
+  @IsString()
+  telefone?: string;
+
+  @IsOptional()
+  @IsString()
+  cargo?: string;
+
+  @IsOptional()
+  @IsString()
+  senha?: string; // nova senha
+
+  @IsOptional()
+  @IsString()
+  senhaAtual?: string; // senha antiga (para conferência)
+
+  @IsOptional()
+  @IsString()
+  foto?: string;
+}
