b2ff1be356d448e47f2d8a5b71d53266dcd2cb01
João Vitor Sartoreto <145504494+JoaoSartoreto@users.noreply.github.com>
2025-07-04 15:34:08 -0400

    allow specifying filename when saving

--- src/documentos/documentos.module.ts
+++ src/documentos/documentos.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Documento } from './entities/documento.entity';
 import { DocumentosService } from './documentos.service';
 import { DocumentosController } from './documentos.controller';
+import { FilesModule } from '../files/files.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Documento]),
+    FilesModule,
   ],
   providers: [DocumentosService],
   controllers: [DocumentosController],
