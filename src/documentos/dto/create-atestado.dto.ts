b4d557b2ff186c0930f5a23fcf19597eae553a03
Makakof <luiz.h.s.silva@ufms.br>
2025-07-08 14:05:48 -0400

    Adiconando checklist no atestado

---
+++ src/documentos/dto/create-atestado.dto.ts
@@ -0,0 +1,33 @@
+import { IsBoolean, IsEnum, IsInt, IsOptional, IsString, Length, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { Status } from 'src/enums/status.enum';
+import { Checklist } from '../models/checklist';
+import { IsArray } from 'class-validator';
+
+export class CreateAtestadoDto {
+  @IsString()
+  @Length(1, 255)
+  caminho: string;
+
+  @IsBoolean()
+  maior3dias: boolean = false;
+
+  @IsBoolean()
+  concluido: boolean = false;
+
+  @IsEnum(Status)
+  status: Status = Status.EM_PROCESSO;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Checklist)
+  checklis: Checklist[];
+
+  @IsString()
+  @IsOptional()
+  @Length(0, 255)
+  justificativa?: string;
+
+  @IsInt()
+  requerimentoId: number;
+}
---
+++ src/documentos/models/checklist.ts
@@ -0,0 +1,11 @@
+export class Checklist {
+  incisoI: boolean;
+  incisoII: boolean;
+  incisoIII: boolean;
+  incisoIV: boolean;
+  incisoV: boolean;
+  incisoVI: boolean;
+  incisoVII: boolean;
+  incisoVIII: boolean;
+  periodoMaiorQue3Dias: boolean;
+}
